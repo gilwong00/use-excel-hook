9697b20357b728aad062fb68395147d0b0e76852
Gilbert Wong <gilwong3105@gmail.com>
2020-06-07 14:27:28 -0700

    initial work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import useExcel from './hooks/useExcel';
 import './App.css';
 
 function App() {
+  const { exportToExcel } = useExcel();
+
+  const handleExport = () => {
+    const data = [{ name: 'test', age: 29 }];
+    exportToExcel({ data });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleExport}>Export to excel</button>
     </div>
   );
 }
---
+++ src/hooks/useExcel.tsx
@@ -0,0 +1,90 @@
+import { Workbook, Column, Cell, Style } from 'exceljs';
+import { saveAs } from 'file-saver';
+
+type Header = Pick<Column, 'header' | 'key'>;
+
+interface IParams {
+  headers?: Array<Header>;
+  data: any;
+  fileName?: string;
+  headerStyles?: Style;
+  rowStyles?: Style;
+}
+
+export default function useExcel() {
+  const defaults = {
+    header: {
+      font: {
+        size: 12
+      }
+    },
+    row: {
+      font: {
+        size: 12
+      }
+    }
+  };
+
+  const generateHeaders = (data: Array<any>): Array<Header> => {
+    if (data && data[0]) {
+      const value = data[0];
+      const headers: Array<Header> = Object.keys(value).map((key) => ({
+        header: key,
+        key
+      }));
+      return headers;
+    } else {
+      return [];
+    }
+  };
+
+  const exportToExcel = async ({
+    headers = [],
+    data = [],
+    fileName = new Date().toISOString(),
+    headerStyles,
+    rowStyles
+  }: IParams): Promise<void> => {
+    if (!data) {
+      throw new Error('data is required');
+    }
+
+    if (!headers.length) {
+      headers = generateHeaders(data);
+    }
+
+    const wb = new Workbook();
+    const ws = wb.addWorksheet();
+    ws.columns = headers as Array<Column>;
+    ws.addRows(data);
+
+    ws.columns.forEach((column) => (column.width = 25));
+    ws.eachRow({ includeEmpty: false }, (row, rowNum) => {
+      // add style to headers only
+      if (rowNum === 1) {
+        row.eachCell((cell: Cell) => {
+          cell.font = headerStyles?.font ?? defaults.header.font;
+
+          if (headerStyles && headerStyles.fill) {
+            cell.fill = headerStyles.fill;
+          }
+        });
+      } else {
+        row.eachCell((cell: Cell) => {
+          cell.font = rowStyles?.font ?? defaults.row.font;
+
+          if (rowStyles && rowStyles.fill) {
+            cell.fill = rowStyles.fill;
+          }
+        });
+      }
+    });
+
+    const buffer = await wb.xlsx.writeBuffer();
+    saveAs(new Blob([buffer]), `${fileName}.xlsx`);
+  };
+
+  return {
+    exportToExcel
+  };
+}
